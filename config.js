7afea16d6233c14e363d4760908bcaad44ab62a9
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-04-24 10:12:49 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+ 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBAjAroVYY8wV86_hTUNfyQaCS47HSAgas",
+    authDomain: "sahiib.firebaseapp.com",
+    projectId: "sahiib",
+    storageBucket: "sahiib.appspot.com",
+    messagingSenderId: "383331050799",
+    appId: "1:383331050799:web:fc7dd2e52d36892caf6dda",
+    measurementId: "G-V0F9XYXB6J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  
